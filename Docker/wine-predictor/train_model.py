263c8315f89161c89c54c0158d459c01815736d4
RabiaA-arif <rabiaarif694@gmail.com>
2026-03-25 23:03:38 +0500

    simple ml project with docker deployment

---
+++ Docker/wine-predictor/train_model.py
@@ -0,0 +1,39 @@
+import pickle
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+
+#  load the wine dataset 
+wine=load_wine()
+X,y=wine.data,wine.target
+
+
+# Split the data 
+
+X_train , X_test ,y_train , y_test =train_test_split(
+    X,y,test_size=0.2,random_state=42
+)
+
+# Scale features 
+scaler=StandardScaler()
+X_train_scaled=scaler.fit_transform(X_train)
+X_test_scaled=scaler.transform(X_test)
+
+# train the model 
+model=RandomForestClassifier(n_estimators=100,random_state=42)
+model.fit(X_train_scaled,y_train)
+
+# Evaluate 
+accuracy=model.score(X_test_scaled,y_test)
+print(f"Model accuracy {accuracy}")
+
+# save both the model and scaler
+
+with open('model.pkl','wb') as f :
+    pickle.dump(model,f)
+    
+with open('scaler.pkl','wb') as f:
+    pickle.dump(scaler,f)
+    
+print("Model and scaler saved successfully ")
