29f98340f9162356d4285441053bcd0f6c6a39e5
Rabia Arif <rabiaarif694@gmail.com>
2026-06-21 22:17:34 +0500

    get method in fastapi

--- FastAPI/fastapi_project/main.py
+++ FastAPI/fastapi_project/main.py
@@ -10,3 +10,13 @@ def greet():
 @app.get("/intro")
 def intro():
     return {"Introduction":"Hi i am Rabia Arif AI Engineer"}
+
+
+@app.get("/customer")
+
+def customer(customer_id: int):
+    return {
+        "customer id":customer_id,
+        "customer_name":"Rabia",
+        "customer_status":"Active"
+    }
