271b5fcc7fe2160089acb461ddf114167ac4d01d
Rabia Arif <rabiaarif694@gmail.com>
2026-06-21 13:40:25 +0500

    building 2 endpoints

--- FastAPI/fastapi_project/Readme.md
+++ FastAPI/fastapi_project/Readme.md
@@ -1,2 +1,8 @@
 # my venv name is "virtual"
 for activation of venc us command `virtual\scripts\activate`
+
+*`uvicorn main:app --reload`*
+uvicorn: is the server
+main : is file name
+app: we declare application in main.py file
+--relaod:when ever we do changes so it will automatically load
--- FastAPI/fastapi_project/main.py
+++ FastAPI/fastapi_project/main.py
@@ -1,7 +1,12 @@
-from fastapi import FASTAPI
+from fastapi import FastAPI
 
-app = FASTAPI()
+app = FastAPI()
 
 @app.get("/")
 def greet():
     return {"message":"welcome to our fastapi project"}
+
+
+@app.get("/intro")
+def intro():
+    return {"Introduction":"Hi i am Rabia Arif AI Engineer"}
