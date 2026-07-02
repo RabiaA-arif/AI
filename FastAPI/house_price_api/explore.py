ca4ba59bf00ddbf5b640e792a0991765d6016a09
Rabia Arif <rabiaarif694@gmail.com>
2026-07-02 22:38:24 +0500

    train model

--- FastAPI/house_price_api/explore.py
+++ FastAPI/house_price_api/explore.py
@@ -1,11 +1,13 @@
-from scikitlearn.datasets import fetch_california_housing
+from sklearn.datasets import fetch_california_housing
 import pandas  as pd
 
 
 data = fetch_california_housing()
-df = pd.DataFrame(data.data,columns=data.feature.names)
+df = pd.DataFrame(data.data,columns=data.feature_names)
 
 df["Price"]  = data.target
-print(f"Shape:{data.shape}")
+print(f"Shape:{df.shape}")
+print("\n")
 print(df.head())
+print("\n")
 print(df.describe())
---
+++ FastAPI/house_price_api/train.py
@@ -0,0 +1,6 @@
+from sklearn import fetch_california_housing
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error,r2_score
+import pandas as pd
+import joblib
