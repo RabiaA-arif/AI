28f4a2972897de3e3c46b53b21c332aa3c3397a0
Rabia Arif <rabiaarif694@gmail.com>
2026-07-02 23:51:57 +0500

    creating form

--- FastAPI/house_price_api/main.py
+++ FastAPI/house_price_api/main.py
@@ -6,3 +6,17 @@ from pydantic import BaseModel , Field
 app = FastAPI()
 model = joblib("house_features.joblib")
 features = joblib("house_features.joblib")
+
+
+# input schema
+
+class HouseFeatures(BaseModel):
+    MedInc : float = Field(gt =0,description="Model Income of"\
+        "Neighbouredhood")
+    HouseAge :float = (Field(gt=0,description="Average age of house"))
+    AveRooms : float = Field(gt=0,description="Average number of rooms")
+    AveBedrooms : float = Field(gt=0,description="Average number of rooms")
+    Population:float = Field(gt=0,description="Total population")
+    AveOccup: float = Field(gt=0,description="Average occupation")
+    Latitude : float = Field(ge=32,le=42,description="Latitude")
+    Longitude : float = Field(ge=125,le=114,description="Longitude")
