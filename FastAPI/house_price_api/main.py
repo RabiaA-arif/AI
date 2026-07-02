b6d4014fc814d87f944014cc28dfcab0345a7e14
Rabia Arif <rabiaarif694@gmail.com>
2026-07-03 00:20:20 +0500

    creating app for home health predicted

--- FastAPI/house_price_api/main.py
+++ FastAPI/house_price_api/main.py
@@ -20,3 +20,51 @@ class HouseFeatures(BaseModel):
     AveOccup: float = Field(gt=0,description="Average occupation")
     Latitude : float = Field(ge=32,le=42,description="Latitude")
     Longitude : float = Field(ge=125,le=114,description="Longitude")
+    
+# home
+@app.get("/")
+def home():
+    return{
+        "message":"California House prediction api",
+        "status":"running",
+        "endpoint":"send POST request to /predict"
+        
+    }
+    
+@app.get("/health")
+def health():
+    return {
+        "status":"running",
+        "model":"RandomForestRegressor",
+        "features":features,
+        "avg_error":"$39,000"
+            
+    }
+    
+# prediction
+@app.post("/predict")
+    
+def predict(house:HouseFeatures):
+    try:
+        input_data =pd.DataFrame([{
+                "MedInc":house.MedInc,
+                "HouseAge":house.HouseAge,
+                "AveRooms":house.AveRooms,
+                "AveBedrms":house.AveBedrooms,
+                "Population":house.Population,
+                "latitude":house.Latitude,
+                "Longitude":house.Longitude
+        }])
+        predicted = model.predict(input)[0]
+        price_usd = predicted * 100000
+    
+        return{
+            "predicted_price":f"${price_usd:,.0f}",
+            "predicted_price_short":f"${predicted:,.2f} hundred thousands",
+            "fidence_range":f"${price_usd - 39000:,.0f} to ${price_usd + 39000:,.0f}"
+        }
+    except Exception as e:
+     raise HTTPException (
+        status_code=500,
+        detail=f"prediction failed :{str(e)}"
+    )
