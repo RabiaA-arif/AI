25624280a6859976624e22755d1bb84c97f92172
Rabia Arif <rabiaarif694@gmail.com>
2026-07-06 23:05:16 +0500

    fastapi practice project

---
+++ FastAPI/practice_project/explore.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import load_breast_cancer
+import pandas as pd
+
+data = load_breast_cancer()
+
+df = pd.DataFrame(data.data,columns = data.feature_names)
+print(df)
+
+print(df["mean radius"])
+
+print(f"Shape of data:{df.shape}")
+
+print("Property of data set")
+
+print(f"Describe:{df.describe()}")
